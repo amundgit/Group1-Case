8c07dc14643d55dadd68c37405ced3a6cc19cc67
AmundF <amundfro@gmail.com>
2018-10-15 13:04:16 +0200

    Server:Reinitialize

---
+++ Serverfiles/src/main/java/hello/GreetingController.java
@@ -0,0 +1,36 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.CookieValue;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import java.util.Arrays;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/")
+    public String greeting(HttpServletResponse response, HttpServletRequest request) {
+    	boolean cookieExist =false;
+    	Cookie[] cookies = request.getCookies();
+    	if (cookies != null) {
+    		for(Cookie c : cookies){
+    			if((c.getName().equals("username"))&&(!c.getValue().equals("default"))){
+    				cookieExist = true;
+    			}
+    		}
+        }
+        if(cookieExist){
+    		return "greeting";
+    	}else{
+    		response.addCookie(new Cookie("username", "default"));
+        	return "login";
+    	}
+    }
+
+}
