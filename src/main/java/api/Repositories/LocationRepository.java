4a4f2354204e82e83395a6b9586de31d2796ed5d
Damoad <daud.mohamed.adam@gmail.com>
2018-10-23 12:50:48 +0200

    added a location post

---
+++ src/main/java/api/Repositories/LocationRepository.java
@@ -0,0 +1,27 @@
+package api.Repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
+
+import api.Models.Location;
+
+import java.util.List;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface LocationRepository extends CrudRepository<Location, Integer> {
+    /*
+     * Kinda mal? User findByName(String name); //test
+     * 
+     * @Query("SELECT u FROM User u WHERE LOWER(u.name) = LOWER(:name) AND LOWER(u.password) = LOWER(:password)"
+     * ) User verifyUser(@Param("name")String name, @Param("password")String
+     * password);
+     */
+
+    @Query("SELECT a FROM Location a WHERE name = :name")
+    Location getByName(@Param("name") String name);
+
+}
