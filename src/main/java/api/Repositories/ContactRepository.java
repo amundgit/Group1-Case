d906354998a7f3e3688822686be9cde91df1d6e8
Damoad <daud.mohamed.adam@gmail.com>
2018-10-24 10:13:28 +0200

    Set up a contact post and changed the others responsebody aswell

--- src/main/java/api/Repositories/ContactRepository.java
+++ src/main/java/api/Repositories/ContactRepository.java
@@ -12,20 +12,19 @@ import java.util.List;
 // CRUD refers Create, Read, Update, Delete
 
 public interface ContactRepository extends CrudRepository<Contact, Integer> {
+
+    @Query("SELECT c FROM Contact c WHERE c.person.person_id = :person_id")
+    List<Contact> findByID(@Param("person_id") Integer person_id);
+
     /*
-     * @Query("SELECT c FROM Contact c WHERE person_id = :person_id") List<Contact>
-     * findByID(@Param("person_id") Integer person_id);
-     * 
-     * @Query("SELECT c FROM Contact c WHERE (person_id) = (:person_id) AND LOWER(contact_type) = \'work\'"
+     * @Query("SELECT c FROM Contact c WHERE (c.person.person_id) = (:person_id) AND LOWER(c.person.contact_type) = \'work\'"
      * ) List<Contact> findByIDandWork(@Param("person_id") Integer person_id);
      * 
-     * @Query("SELECT c FROM Contact c WHERE (person_id) = (:person_id) AND LOWER(contact_type) = \'home\'"
+     * @Query("SELECT c FROM Contact c WHERE (c.person.person_id) = (:person_id) AND LOWER(c.person.contact_type) = \'home\'"
      * ) List<Contact> findByIDandHome(@Param("person_id") Integer person_id);
-     * 
-     * @Query("SELECT c FROM Contact c WHERE (person_id) = (:person_id) AND LOWER(contact_type) = LOWER(:contact_type) and LOWER(contact_detail) and LOWER(:contact_detail)"
-     * ) Contact findByIDandTypeandDetails(@Param("person_id") Integer
-     * person_id, @Param("contact_type") String contact_type,
-     * 
-     * @Param("contact_detail") String contact_detail);
      */
+    @Query("SELECT c FROM Contact c WHERE (c.person.person_id) = (:person_id) AND LOWER(c.person.contact_type) = LOWER(:contact_type) and LOWER(c.person.contact_detail) and LOWER(:contact_detail)")
+    Contact findByIDandTypeandDetails(@Param("person_id") Integer person_id, @Param("contact_type") String contact_type,
+            @Param("contact_detail") String contact_detail);
+
 }
