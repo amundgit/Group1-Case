920120aa3e160d501ea5d08ebf192b60e96b3404
Damoad <daud.mohamed.adam@gmail.com>
2018-10-24 10:40:18 +0200

    Set up a contact post and changed the others responsebody aswell

--- src/main/java/api/Repositories/ContactRepository.java
+++ src/main/java/api/Repositories/ContactRepository.java
@@ -22,7 +22,7 @@ public interface ContactRepository extends CrudRepository<Contact, Integer> {
     @Query("SELECT c FROM Contact c WHERE (c.person.person_id) = (:person_id) AND LOWER(contact_type) = \'home\'")
     List<Contact> findByIDandHome(@Param("person_id") Integer person_id);
 
-    @Query("SELECT c FROM Contact c WHERE (c.person.person_id) = (:person_id) AND LOWER(contact_type) = LOWER(:contact_type) AND LOWER(contact_detail) AND LOWER(:contact_detail)")
+    @Query("SELECT c FROM Contact c WHERE (c.person.person_id) = (:person_id) AND LOWER(contact_detail) AND LOWER(:contact_detail)")
     Contact findByIDandTypeandDetails(@Param("person_id") Integer person_id, @Param("contact_type") String contact_type,
             @Param("contact_detail") String contact_detail);
 
