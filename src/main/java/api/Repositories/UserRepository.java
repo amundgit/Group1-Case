889f2f8467cea4934c20428fb49027fa29af1311
AmundF <amundfro@gmail.com>
2018-10-24 13:48:33 +0200

    more mergefix

--- src/main/java/api/Repositories/UserRepository.java
+++ src/main/java/api/Repositories/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 
 import api.Models.User;
+import api.Pojos.SessionInfo;
 
 import java.util.List;
 
@@ -14,8 +15,14 @@ import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 	
-	User findByName(String name); //test
+	//Check if username exist
+	User findByName(String name);
+
 
 	@Query("SELECT u FROM User u WHERE LOWER(u.name) = LOWER(:name) AND LOWER(u.password) = LOWER(:password)")
 	User verifyUser(@Param("name")String name, @Param("password")String password);
+
+	//Get username and sessionid by username.
+  	@Query("select new api.Pojos.SessionInfo(u.name, u.sessionId) from User u where u.name = ?1")
+	SessionInfo findSessionByName(String name);
 }
