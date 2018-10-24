c54e6394608c07fd3763a301499056288b2ff0fe
AmundF <amundfro@gmail.com>
2018-10-24 13:51:16 +0200

    mergefix

--- src/main/java/api/Repositories/AddressRepository.java
+++ src/main/java/api/Repositories/AddressRepository.java
@@ -9,14 +9,21 @@ import api.Models.Address;
 
 import java.util.List;
 
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
 
 public interface AddressRepository extends CrudRepository<Address, Integer> {
-	/*Kinda mal?
-	User findByName(String name); //test
-
-	@Query("SELECT u FROM User u WHERE LOWER(u.name) = LOWER(:name) AND LOWER(u.password) = LOWER(:password)")
-	User verifyUser(@Param("name")String name, @Param("password")String password);*/
+	/*
+	 * Kinda mal? User findByName(String name); //test
+	 * 
+	 * @Query("SELECT u FROM User u WHERE LOWER(u.name) = LOWER(:name) AND LOWER(u.password) = LOWER(:password)"
+	 * ) User verifyUser(@Param("name")String name, @Param("password")String
+	 * password);
+	 */
 
 	@Query("SELECT a FROM Address a WHERE address_id = :id")
-	Address getById(@Param("id")Integer id);
+	Address getById(@Param("id") Integer id);
+
+	@Query("SELECT a FROM Address a WHERE LOWER(address_line_1) = LOWER(:address)")
+	Address getByAddress(@Param("address") String address);
 }
--- src/main/java/api/Repositories/LocationRepository.java
+++ src/main/java/api/Repositories/LocationRepository.java
@@ -9,11 +9,19 @@ import api.Models.Location;
 
 import java.util.List;
 
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
 
 public interface LocationRepository extends CrudRepository<Location, Integer> {
-	/*Kinda mal?
-	User findByName(String name); //test
+    /*
+     * Kinda mal? User findByName(String name); //test
+     * 
+     * @Query("SELECT u FROM User u WHERE LOWER(u.name) = LOWER(:name) AND LOWER(u.password) = LOWER(:password)"
+     * ) User verifyUser(@Param("name")String name, @Param("password")String
+     * password);
+     */
 
-	@Query("SELECT u FROM User u WHERE LOWER(u.name) = LOWER(:name) AND LOWER(u.password) = LOWER(:password)")
-	User verifyUser(@Param("name")String name, @Param("password")String password);*/
+    @Query("SELECT a FROM Location a WHERE LOWER(name) = LOWER(:name)")
+    Location getByName(@Param("name") String name);
+
 }
