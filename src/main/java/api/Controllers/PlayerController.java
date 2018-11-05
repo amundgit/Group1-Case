be080020a16f0ffc1f34b8964102d8f08951c6bb
Damoad <daud.mohamed.adam@gmail.com>
2018-11-05 14:20:26 +0100

    updated player assign to transfer on existance

--- src/main/java/api/Controllers/PlayerController.java
+++ src/main/java/api/Controllers/PlayerController.java
@@ -45,8 +45,9 @@ public class PlayerController {
 	@PostMapping(path = "/assign")
 	public @ResponseBody Messages addPlayer(@RequestBody Map<String, Object> body) {
 		Messages m = new Messages();
-		m = SecurityUtil.verifySession(body.get("sessionid").toString(), body.get("sessionuser").toString(),userRepository);
-		if(m.getRole() != 1) {
+		m = SecurityUtil.verifySession(body.get("sessionid").toString(), body.get("sessionuser").toString(),
+				userRepository);
+		if (m.getRole() != 1) {
 			return m;
 		} else {
 			boolean check = false;
@@ -55,7 +56,7 @@ public class PlayerController {
 			Integer person_id = Integer.parseInt(body.get("person_id").toString());
 			String team_id = body.get("team_id").toString();
 			Player existenceCheck = playerRepository.getByPersonId(person_id);
-			//Actually do stuff
+			// Actually do stuff
 			if (existenceCheck == null) {
 				check = true;
 			}
@@ -68,7 +69,11 @@ public class PlayerController {
 				playerRepository.save(p);
 				m.setMessage("Success");
 			} else {
-				m.setError("Error: Person already a player");
+				existenceCheck.setNumber(number);
+				existenceCheck.setNormalPosition(normal_position);
+				existenceCheck.setTeamId(teamRepository.getByTeamId(team_id));
+				playerRepository.save(existenceCheck);
+				m.setMessage("Updated");
 			}
 			return m;
 		}
