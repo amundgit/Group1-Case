30193ffd009cbd3150699ff65749de9a0ab76678
Damoad <daud.mohamed.adam@gmail.com>
2018-11-06 14:05:40 +0100

    get players by team

--- src/main/java/api/Models/Player.java
+++ src/main/java/api/Models/Player.java
@@ -11,8 +11,8 @@ import javax.persistence.*;//backup
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity // This tells Hibernate to make a table out of this class
-public class Player{
-	@Id
+public class Player {
+  @Id
   @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
   @GenericGenerator(name = "native", strategy = "native")
   private Integer player_id;
@@ -21,21 +21,22 @@ public class Player{
 
   private Integer number;
 
-  //FOREIGN KEYS-------------------------------------
-  @ManyToOne(fetch = FetchType.EAGER, optional=false)
+  // FOREIGN KEYS-------------------------------------
+  @ManyToOne(fetch = FetchType.EAGER, optional = false)
   @JoinColumn(name = "person_id", nullable = false)
   private Person person;
 
-  @ManyToOne(fetch = FetchType.EAGER, optional=false)
+  @ManyToOne(fetch = FetchType.EAGER, optional = false)
   @JoinColumn(name = "team_id", nullable = false)
   private Team team;
-  //-------------------------------------------------
-	private String status = "active";
+  // -------------------------------------------------
+  private String status = "active";
 
-  //GETTERS AND SETTERS
+  // GETTERS AND SETTERS
   public Integer getId() {
     return player_id;
   }
+
   public void setId(Integer player_id) {
     this.player_id = player_id;
   }
@@ -43,6 +44,7 @@ public class Player{
   public String getNormalPosition() {
     return normal_position;
   }
+
   public void setNormalPosition(String normal_position) {
     this.normal_position = normal_position;
   }
@@ -50,13 +52,16 @@ public class Player{
   public Integer getNumber() {
     return number;
   }
+
   public void setNumber(Integer number) {
     this.number = number;
   }
-  //FOREIGN KEYS-------------------------------------
+
+  // FOREIGN KEYS-------------------------------------
   public Integer getPersonId() {
     return person.getId();
   }
+
   public void setPersonId(Person person) {
     this.person = person;
   }
@@ -64,30 +69,30 @@ public class Player{
   public String getTeamId() {
     return team.getId();
   }
+
   public void setTeamId(Team team) {
     this.team = team;
   }
-  //-------------------------------------------------
+
+  // -------------------------------------------------
   public String getStatus() {
     return status;
   }
+
   public void setStatus(String status) {
     this.status = status;
   }
+
+  public Person getPerson() {
+    return person;
+  }
 }
 
-//SQL:
+// SQL:
 /*
-CREATE TABLE PLAYER
-(
-  player_id INTEGER NOT NULL,
-  normal_position VARCHAR(64),
-  number VARCHAR(8),
-  person_id INTEGER NOT NULL,
-  team_id INTEGER NOT NULL,
-  status VARCHAR(64) DEFAULT 'Active',
-  PRIMARY KEY (player_id),
-  FOREIGN KEY (person_id) REFERENCES PERSON(person_id),
-  FOREIGN KEY (team_id) REFERENCES TEAM(team_id)
-);
-*/
+ * CREATE TABLE PLAYER ( player_id INTEGER NOT NULL, normal_position
+ * VARCHAR(64), number VARCHAR(8), person_id INTEGER NOT NULL, team_id INTEGER
+ * NOT NULL, status VARCHAR(64) DEFAULT 'Active', PRIMARY KEY (player_id),
+ * FOREIGN KEY (person_id) REFERENCES PERSON(person_id), FOREIGN KEY (team_id)
+ * REFERENCES TEAM(team_id) );
+ */
